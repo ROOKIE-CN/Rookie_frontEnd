e2a33c45189ab7e27ac2dbffe7db91d07a7c00a4
cn.c <2927890180@qq.com>
2020-10-23 19:58:35 +0800

    2

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- element_ui测试 -->
+    <div class="element_ui">
+      <el-progress :percentage="50"></el-progress>
+      <el-progress :percentage="100" :format="format"></el-progress>
+      <el-progress :percentage="100" status="success"></el-progress>
+      <el-progress :percentage="100" status="warning"></el-progress>
+      <el-progress :percentage="50" status="exception"></el-progress>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      format(percentage) {
+        return percentage === 100 ? '满' : `${percentage}%`;
+      }
+    }
+  };
+</script>
+
+<style  scoped>
+/* @import "~common/stylus/variable.styl" */
+</style>
