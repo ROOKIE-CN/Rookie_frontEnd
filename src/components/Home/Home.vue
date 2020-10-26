589ba0c58fb7fb9815044f760c9a466e42a2e025
e <3207939990@qq.com>
2020-10-26 19:11:27 +0800

    第十三

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
+  <div class="add">
     <!-- element_ui测试 -->
-    <div class="element_ui">
-      <el-progress :percentage="50"></el-progress>
-      <el-progress :percentage="100" :format="format"></el-progress>
-      <el-progress :percentage="100" status="success"></el-progress>
-      <el-progress :percentage="100" status="warning"></el-progress>
-      <el-progress :percentage="50" status="exception"></el-progress>
-    </div>
+    <el-row :gutter="10">
+    <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
+    <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
+  </el-row>
+
     <!-- stylus测试 -->
     <div class="stylus">
       1231231111111111111111111111
@@ -21,7 +21,6 @@
 import Header from './Header'
   export default {
     methods: {
-
     },
     components: {
       Header
@@ -29,7 +28,21 @@ import Header from './Header'
   };
 </script>
 
-<style lang="stylus" scoped>
-.stylus
-  font-size 100px
+<style scoped>
+ .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,24 @@ import store from './store'
 
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
-
 import './assets/style/reset.css'
-// import 'stylus'
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app');
 
+
+import axios from 'axios'
+axios.defaults.baseURL = "http://192.168.137.1:25575/"
+import VueAxios from 'vue-axios'
+// import axios from "axios";
+// import api from './http/api';
+// import http from './http/http';
+// // axios 拦截器
+// import './http/axios'
+
+// // 对后端接口 进行全局注册
+// Vue.prototype.$api = api;
+// // 对请求方式 进行全局注册
+// Vue.prototype.$http = http;
+createApp(App).use(store).use(router).use(ElementPlus).use(VueAxios, axios).mount('#app');
+axios.get("/user/test")
+.then(res => {
+  console.log(res)
+})
